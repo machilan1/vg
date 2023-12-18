f33853a74461f5750583f37f719756c97caa23c7
machilan1 <machilan1@gmail.com>
2023-12-19 02:01:19 +0800

    feat : basic erd

--- libs/api/database/src/lib/schema.ts
+++ libs/api/database/src/lib/schema.ts
@@ -6,15 +6,12 @@ import {
   varchar,
 } from 'drizzle-orm/pg-core';
 
-export const roleEnum = pgEnum('role', ['guest', 'student', 'teacher']);
-
 export const user = pgTable('users', {
   userId: bigserial('user_id', { mode: 'number' }).primaryKey().notNull(),
-  role: roleEnum('role').default('student').notNull(),
   name: varchar('name', { length: 45 }).notNull().unique(),
   email: varchar('email', { length: 45 }).notNull().unique(),
   avatar: varchar('avatar', { length: 255 }),
-  password: varchar('password').notNull(),
+  password: varchar('password', { length: 45 }),
   createdAt: timestamp('created_at', { withTimezone: true })
     .defaultNow()
     .notNull(),
