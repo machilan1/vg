ce65f5ba2c15a202e7135a3b064440a0498b680a
machilan1 <machilan1@gmail.com>
2023-12-21 04:03:06 +0800

    feat:(api) : remove unnecessary code

--- libs/api/auth/src/lib/auth.module.ts
+++ libs/api/auth/src/lib/auth.module.ts
@@ -2,14 +2,13 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { UsersService } from '@vg/api-users';
-import { DatabaseModule } from '@vg/api-database';
 import { AuthService } from './auth.service';
 
 @Global()
 @Module({
   controllers: [AuthController],
   providers: [ConfigService, UsersService, AuthService],
-  imports: [DatabaseModule],
+  imports: [],
   exports: [AuthService],
 })
 export class AuthModule {}
--- libs/api/categories/src/lib/categories.module.ts
+++ libs/api/categories/src/lib/categories.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
-import { DatabaseModule } from '@vg/api-database';
 
 @Module({
   controllers: [CategoriesController],
   providers: [CategoriesService],
-  imports: [DatabaseModule],
+  imports: [],
   exports: [],
 })
 export class CategoriesModule {}
